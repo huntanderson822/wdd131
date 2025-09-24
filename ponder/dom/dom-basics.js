9291478100fe7daff6d74902236255ed47616343
huntanderson822 <and25027@byui.edu>
2025-09-23 19:40:28 -0600

    Worked on DOM basics

---
+++ ponder/dom/dom-basics.js
@@ -0,0 +1,16 @@
+const newParagraph = document.createElement("p");
+newParagraph.innerText = "Added with Javascript!";
+document.body.appendChild(newParagraph);
+
+const newImage = document.createElement("img");
+newImage.setAttribute("src", "https://picsum.photos/200");
+newImage.setAttribute("alt", "Random image from picsum.");
+document.body.appendChild(newImage);
+
+const newDiv = document.createElement("div");
+newDiv.innerHTML = "<ul><li>One</li><li>Two</li><li>Three</li></ul>";
+document.body.appendChild(newDiv);
+
+const newSection = document.createElement("section");
+newSection.innerHTML = "<h2>Java Script is super easy!</h2><p>I'm lying, but I'm learning.</p>";
+document.body.appendChild(newSection);
